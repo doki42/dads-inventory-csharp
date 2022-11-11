ec74b6315fb48f99dd09d89d634f20256c94944a
appuser <appuser@journey2-6cb8b97c58-dm5zs>
2022-11-11 06:47:03 +0000

    starter project

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using DadsInventory.Models;
+using DadsInventory.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DadsInventory.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ItemController(IItemRepository itemRepository, ICategoryRepository categoryRepository)
+        {
+            _itemRepository = itemRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List(string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+                searchTerm = "";
+
+            var itemsListViewModel = new ItemsListViewModel
+            {
+                Items = _itemRepository.AllItems.Where(x => x.Name.Contains(searchTerm)), Title = "Dad's Inventory"
+            };
+
+
+            return View(itemsListViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var item = _itemRepository.GetItemById(id);
+            return item == null ? (IActionResult) NotFound() : View(item);
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DadsInventory.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Category>().HasData(new Category {CategoryId = 4, CategoryName = "Large"});
+            modelBuilder.Entity<Category>().HasData(new Category {CategoryId = 5, CategoryName = "Medium"});
+            modelBuilder.Entity<Category>().HasData(new Category {CategoryId = 6, CategoryName = "Small"});
+
+            modelBuilder.Entity<Item>().HasData(
+                new Item {Id = 1, Name = "Ship from fb", InStock = true, CategoryId = 4, Price = 199.95m},
+                new Item {Id = 2, Name = "Hammer from db", InStock = false, CategoryId = 5, Price = 7.95m},
+                new Item {Id = 3, Name = "Pin from db", InStock = true, CategoryId = 6, Price = 1.95m}
+            );
+        }
+    }
+}
