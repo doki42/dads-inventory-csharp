8f4868730d59bbdbfdf739a5a6eb1de1c9bfb206
doki42 <andras.szogi@gmail.com>
2022-11-16 15:15:52 +0100

    Add role management

---
+++ Models/RoleEdit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace DadsInventory.Models;
+
+public class RoleEdit
+{
+    public IdentityRole Role { get; set; }
+    public IEnumerable<User> Members { get; set; }
+    public IEnumerable<User> NonMembers { get; set; }
+}
